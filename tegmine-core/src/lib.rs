b6efcd75fff7bf518bb168bb0eb99bfd28a2e933
starryflow <opensource@starryflow.com>
2023-06-05 19:09:49 +0800

    feat: supports parallel execution of workflows by the thread pool

--- tegmine-core/src/lib.rs
+++ tegmine-core/src/lib.rs
@@ -9,11 +9,13 @@ mod runtime;
 mod service;
 mod utils;
 
+use crossbeam_channel::Sender;
 pub use model::{TaskModel, TaskStatus, WorkflowModel, WorkflowStatus};
 pub use runtime::{
     SystemTaskRegistry, TaskMapper, TaskMapperContext, TaskMapperRegistry, WorkflowSystemTask,
 };
 pub use service::{ExecutionService, MetadataService, TaskService, WorkflowService};
+use tegmine_common::prelude::InlineStr;
 pub use utils::ParametersUtils;
 
 pub fn initialize() {
@@ -22,7 +24,7 @@ pub fn initialize() {
 
 pub fn spawn_event_loop() {
     std::thread::spawn(|| loop {
-        runtime::Channel::handle_evaluation_event()
+        runtime::Channel::handle_evaluation_event_paralle()
     });
 
     std::thread::spawn(|| loop {
@@ -32,6 +34,13 @@ pub fn spawn_event_loop() {
 
 pub fn evaluate_once() -> tegmine_common::prelude::TegResult<()> {
     runtime::Channel::evaluate_once()
+}
+
+pub fn block_execute_workflow(
+    start_workflow_request: tegmine_common::StartWorkflowRequest,
+    sender: Sender<()>,
+) -> tegmine_common::prelude::TegResult<InlineStr> {
+    runtime::Channel::block_execute(start_workflow_request, sender)
 }
 
 #[cfg(test)]
--- tegmine-core/src/runtime/execution/channels.rs
+++ tegmine-core/src/runtime/execution/channels.rs
@@ -1,10 +1,13 @@
 use crossbeam_channel::{Receiver, Sender};
+use dashmap::{DashMap, DashSet};
+use futures::executor::{ThreadPool, ThreadPoolBuilder};
 use tegmine_common::prelude::*;
+use tegmine_common::StartWorkflowRequest;
 
 use super::WorkflowExecutor;
 use crate::runtime::event::{WorkflowCreationEvent, WorkflowEvaluationEvent};
 use crate::runtime::StartWorkflowOperation;
-
+use crate::WorkflowService;
 pub static CREATE_EVENT_CHANNEL: Lazy<(
     Sender<WorkflowCreationEvent>,
     Receiver<WorkflowCreationEvent>,
@@ -15,6 +18,16 @@ pub static EVAL_EVENT_CHANNEL: Lazy<(
     Receiver<WorkflowEvaluationEvent>,
 )> = Lazy::new(|| crossbeam_channel::unbounded());
 
+const THREAD_POOL_SIZE: usize = 20;
+static POOL: Lazy<ThreadPool> = Lazy::new(|| {
+    ThreadPoolBuilder::new()
+        .pool_size(THREAD_POOL_SIZE)
+        .create()
+        .unwrap()
+});
+
+pub static WAITING_QUEUE: Lazy<DashMap<InlineStr, Sender<()>>> = Lazy::new(|| DashMap::new());
+
 pub struct Channel;
 
 impl Channel {
@@ -24,9 +37,18 @@ impl Channel {
         }
     }
 
-    pub fn handle_evaluation_event() {
+    pub fn handle_evaluation_event_paralle() {
         if let Ok(wee) = EVAL_EVENT_CHANNEL.1.recv() {
-            let _ = WorkflowExecutor::handle_workflow_evaluation_event(wee);
+            POOL.spawn_ok(async {
+                let id = wee.workflow_model.workflow_id.clone();
+                let _ = WorkflowExecutor::handle_workflow_evaluation_event(wee);
+
+                // notify caller if using block_execute
+                if let Some(sender) = WAITING_QUEUE.get(&id) {
+                    sender.value().send(());
+                }
+                WAITING_QUEUE.remove(&id);
+            });
         }
     }
 
@@ -37,5 +59,14 @@ impl Channel {
             .map_err(|_| ErrorCode::NotFound("Evaluation Event not found"))?;
         WorkflowExecutor::handle_workflow_evaluation_event(wee)?;
         Ok(())
+    }
+
+    pub fn block_execute(
+        request: StartWorkflowRequest,
+        sender: Sender<()>,
+    ) -> TegResult<InlineStr> {
+        let workflow_instance_id = WorkflowService::start_workflow(request)?;
+        WAITING_QUEUE.insert(workflow_instance_id.clone(), sender);
+        Ok(workflow_instance_id)
     }
 }
